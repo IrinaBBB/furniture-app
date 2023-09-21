bc090d4895c612ac4892dede6fed1c82549d1d1b
IrinaBBB <balkarovairina@gmail.com>
2023-09-21 21:40:57 +0200

    Add custom fonts

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
+import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+import { useCallback } from 'react'
 
 export default function App() {
+    const [fontsLoaded] = useFonts({
+        regular: require('./assets/fonts/Poppins-Regular.ttf'),
+        light: require('./assets/fonts/Poppins-Light.ttf'),
+        bold: require('./assets/fonts/Poppins-Bold.ttf'),
+        medium: require('./assets/fonts/Poppins-Medium.ttf'),
+        extra_bold: require('./assets/fonts/Poppins-ExtraBold.ttf'),
+        semi_bold: require('./assets/fonts/Poppins-SemiBold.ttf'),
+    })
+
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+            await SplashScreen.hideAsync()
+        }
+    }, [fontsLoaded])
+
+    if (!fontsLoaded) {
+        return null
+    }
     return (
         <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
+            <Text style={styles.textStyle}>
+                Open up App.js to start working on your app!
+            </Text>
             <StatusBar style="auto" />
         </View>
     )
@@ -16,5 +39,9 @@ const styles = StyleSheet.create({
         backgroundColor: '#ff1199',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    textStyle: {
+        fontFamily: 'bold',
+        fontSize: 30,
     },
 })
